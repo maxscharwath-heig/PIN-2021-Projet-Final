faed7ddd995b3032267f0e016acfd098d3758924
Nicolas Crausaz <n.crausaz99@gmail.com>
2021-09-06 22:41:53 +0200

    Validate

--- src/objects/Robot.cpp
+++ src/objects/Robot.cpp
@@ -50,22 +50,11 @@ RobotData* Robot::predict(WorldWidget* widget, double deltaTime) {
          vg = leftSpeed,
          vd = rightSpeed;
 
-   /** THE FORMULA ON DOC IS COMPLETELY FU**ED UP... SORRY NOT SORRY
-    * pMax and pMin are the same value so it just force speed to be these value so
-    * the robot go always in linear movement...
-   {//bounding wheel speed
-      double
-            maxS = double(widget->canvas->pMax) / widget->canvas->tMaxV,
-            minS = double(widget->canvas->pMin) / widget->canvas->tMaxV;
-      vg = fmax(minS, fmin(abs(vg), maxS)) * ((vg < 0) ? -1.0 : 1.0);
-      vd = fmax(minS, fmin(abs(vd), maxS)) * ((vd < 0) ? -1.0 : 1.0);
-   }
-    **/
-
-   if (vg == vd) {//linear
+   if (vg == vd) { //linear
       dX = vg * cos(a) * t;
       dY = vg * sin(a) * t;
-   } else {//circle
+   }
+   else { //circle
       double R = radius * (vg + vd) / (vg - vd);
       double w = (vg - vd) / (radius * 2.0) * t;
       dX = R * (cos(a) * sin(w) - sin(a) * (1 - cos(w)));
@@ -106,8 +95,8 @@ void Robot::update(WorldWidget* widget, double deltaTime) {
             toDestroy.push_back(item);
             setEvent(RobotEventState::NO_PARTICULE);
          } else {
-            //stop();
-            rotate(2, getAlignementWithParticle(item) + orientation);
+            stop();
+            rotate(1, getAlignementWithParticle(item) + orientation);
          }
       }
    }
@@ -118,7 +107,6 @@ void Robot::update(WorldWidget* widget, double deltaTime) {
       Logger::Log(LoggerType::P_COLLISION, ss.str());
       aspirate(item);
    }
-
 }
 
 bool Robot::collision(RobotData* a, RobotData* b) {
@@ -216,15 +204,12 @@ Robot::goToPosition(int speed, Position destination) {
 
    double vd = speed;
    double vg = ((R + radius) / (R - radius) * speed);
-   //Limit wheel speed
+   // Limit wheel speed
    limitWheelConstraint(vg, vd);
 
    double w = (vg - vd) / (2 * radius);
    double t = w == 0 ? distance / vg : (2.0 * asin((distance / 2.0) / R)) / w;
    double newAngle = to_deg(w * t);
-   std::cout << "Robot go to " << destination.x << ":" << destination.y
-             << ", arriving in " << t << " s with an angle of " << newAngle << "deg"
-             << std::endl;
 
    return std::make_tuple(vg, vd, t);
 }
