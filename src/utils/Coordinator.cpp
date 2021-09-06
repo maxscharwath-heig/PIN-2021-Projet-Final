61edbb44094e1429e196982eeaf79514a7be7f02
maxscharwath <maxscharwath@gmail.com>
2021-09-06 14:53:48 +0200

    add possible logic in Coordinator.cpp

--- src/utils/Coordinator.cpp
+++ src/utils/Coordinator.cpp
@@ -5,4 +5,23 @@ Coordinator::Coordinator(World* world) : world(world) {
 }
 
 void Coordinator::update() {
+   for (const auto& robot: world->robots) {
+      switch (robot->getEvent()) {
+         case RobotEvent::PARTICULE_CONTACT:
+            //need to orientate to right direction (check if is targeted ?)
+            robot->resetEvent();
+            break;
+         case RobotEvent::NO_PARTICULE:
+            //find closest particule and add to target
+            robot->resetEvent();
+            break;
+         case RobotEvent::COLLISION_WARNING:
+            //rotate or change direction
+            robot->resetEvent();
+            break;
+         default:
+         case RobotEvent::UNKNOWN:
+            break;
+      }
+   }
 }
